ff9fb6381202df6615d68746858a3e3b7982a664
krn-dell-lpt <cse.karanarora@gmail.com>
2024-01-01 12:00:00 +0530

    max in array

---
+++ challange-01-maths/2_maths_maxInArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    vector<int> v;
+
+    v.push_back(21);
+    v.push_back(211);
+    v.push_back(1);
+    v.push_back(2);
+
+    int mx = INT_MIN;
+    for(int i=0;i<v.size();i++){
+        mx = max(mx,v[i]);
+    }
+
+    cout << "max val :- " << mx << endl;
+    
+    return 0;
+
+}
