1177f1d7aeac06b8e01b0937fff36599d4cdc966
krn-dell-lpt <cse.karanarora@gmail.com>
2024-01-01 12:00:00 +0530

    Your commit message

---
+++ challange-01-maths/maths.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int n1 = 45;
+    int n2 = 34;
+    int mx = max(n1,n2);
+
+    cout<< "max val :- " << mx;
+
+    return 0;
+
+}
