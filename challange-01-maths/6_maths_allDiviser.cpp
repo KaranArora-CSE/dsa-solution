373689c8421295140b1e86a16f77e28f30c4cc66
krn-dell-lpt <cse.karanarora@gmail.com>
2024-01-02 12:00:00 +0530

    all divisor

---
+++ challange-01-maths/6_maths_allDiviser.cpp
@@ -0,0 +1,32 @@
+// 🎯   Key Note is go till root and 
+//      just find other number via division
+
+// 📌 App1 - Traverse all
+// 📌 App1 - Traverse till root(N)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int num = 36;
+    vector<int> v;
+
+    for(int i=1;i*i<=num;i++){
+        if(num%i==0) {
+            int tmp = num/i;
+            v.push_back(i);
+            if(v.back()!=tmp) {
+                v.push_back(tmp);
+            }
+        }
+    }
+
+    cout<<"Answer :- ";
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++) cout<<v[i]<<" "; 
+    return 0;
+}
+
+// Input 36 
+// Answer :- 1 2 3 4 6 9 12 18 36
