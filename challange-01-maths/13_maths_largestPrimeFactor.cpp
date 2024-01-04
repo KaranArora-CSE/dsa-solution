88fb669b084409403430094fcc77e544dbe34ac2
krn-dell-lpt <cse.karanarora@gmail.com>
2024-01-04 12:00:00 +0530

    smallest prime factor

---
+++ challange-01-maths/13_maths_largestPrimeFactor.cpp
@@ -0,0 +1,42 @@
+// 📅   10 April 2024
+// 🚨   Lecture 6 - striver Maths 
+
+// 📌 App1 - check for all 
+// 📌 App2 - seive of erastosis 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int start = 50;
+    int end = 700;
+
+    vector<int> v(end+1,1);
+
+    for(int i=2;i<end;i++){
+        if(v[i]==1){
+            int k = i+i;
+            while(k<=end){
+                // if(v[k]==1){
+                v[k]=i;
+                // }
+                k = k+i;
+            }
+        }
+    }
+    // verifying answer
+    cout << "Answers :- " << endl ; 
+    cout << v[354] << endl;
+    cout << v[351] << endl;
+    cout << v[537] << endl;
+    cout << v[539] << endl;
+
+    /* pirintting updated
+    
+    for(int i=1;i<=end;i++){
+        cout << i << " " << v[i];
+        cout << endl;
+    }*/
+    
+    return 0;
+}
--- challange-01-maths/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-int k = i;
-            // while(k<v.size()){
-            //     v[k] = 1;
-            //     k = k+i;
-            // }
